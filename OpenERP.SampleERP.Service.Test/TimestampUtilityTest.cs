b4346c8ad22fa9252adbe23ca9de26d184122eba
Hamed Nargesi <hamedna@systemgroup.net>
2025-09-29 13:05:33 +0330

    TimestampUtility

---
+++ OpenERP.SampleERP.Service.Test/TimestampUtilityTest.cs
@@ -0,0 +1,39 @@
+﻿using AbrPlus.Integration.OpenERP.SampleERP.Shared;
+
+namespace AbrPlus.Integration.OpenERP.SampleERP.Test;
+
+public class TimestampUtilityTest
+{
+    [Fact]
+    public void TimestampToLong_Test()
+    {
+        var data = new byte[] { 0x7F, 0xFF, 0x00, 0x0F, 0xF0, 0xFF, 0x00, 0x0F };
+        long shuoildBe = 0x7FFF000FF0FF000F;
+
+        var result = data.TimestampToLong();
+
+        Assert.Equal(shuoildBe, result);
+    }
+
+    [Fact]
+    public void TimestampToLong_Negative_Test()
+    {
+        var data = new byte[] { 0xF0, 0xFF, 0x00, 0x0F, 0xF0, 0xFF, 0x00, 0x0F };
+        var shuoildBe = unchecked((long)0xF0FF000FF0FF000F);
+
+        var result = data.TimestampToLong();
+
+        Assert.Equal(shuoildBe, result);
+    }
+
+    [Fact]
+    public void TimestampToString_Negative_Test()
+    {
+        var data = new byte[] { 0xF0, 0xFF, 0x00, 0x0F, 0xF0, 0xFF, 0x00, 0x0F };
+        var shuoildBe = unchecked((long)0xF0FF000FF0FF000F);
+
+        var result = data.TimestampToString();
+
+        Assert.Equal(shuoildBe.ToString(), result);
+    }
+}
---
+++ OpenERP.SampleERP/Shared/TimestampUtility.cs
@@ -0,0 +1,22 @@
+﻿namespace AbrPlus.Integration.OpenERP.SampleERP.Shared;
+
+public static class TimestampUtility
+{
+    public static string TimestampToString(this byte[] timestamp)
+    {
+        return timestamp.TimestampToLong().ToString();
+    }
+
+    public static long TimestampToLong(this byte[] timestamp)
+    {
+        long value = 0;
+
+        foreach (var part in timestamp)
+        {
+            value <<= 8;
+            value |= part;
+        }
+
+        return value;
+    }
+}
